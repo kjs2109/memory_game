7c205d3acd6f6294a9e1e722fe9e6ec69f1bc876
kjs2109 <kimjusung2109@naver.com>
2022-01-28 18:59:38 +0900

    2_start_screen

---
+++ 2_start_screen.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+def display_start_screen():
+    pygame.draw.circle(screen, WHITE, start_button.center, 60, 5)
+
+
+pygame.init()
+
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Memory Game')
+
+# 시작 버튼
+start_button = pygame.Rect(0, 0, 120, 120)
+start_button.center = (120, screen_height - 120)
+
+# 색깔
+BLACK = (0, 0, 0)
+WHITE = (225, 225, 225)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 화면 전체를 까맣게 칠함
+    screen.fill(BLACK)
+
+    # 시작 화면 표시
+    display_start_screen()
+
+    # 화면 업데이트
+    pygame.display.update()
+
+pygame.quit()
